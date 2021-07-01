e054bfac2f47aa2b48cbbdda1c3496c9b6dd5472
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-07-01 01:44:18 +0100

    Add basic tests

---
+++ test/canary_test.exs
@@ -0,0 +1,9 @@
+defmodule Napolleon.BaseTest do
+  use ExUnit.Case, async: true
+
+  describe "Making sure testing suite is properly set up" do
+    test "addition" do
+      assert 1 + 1 == 2
+    end
+  end
+end
