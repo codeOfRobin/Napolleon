00b6f73d3e193e8cf16c951fea475a8127df8a3a
Robin Malhotra <me@rmalhotra.com>
2021-07-05 19:32:43 +0100

    Fix module names

--- test/napolleon/http_request_maker/http_request_maker_test.exs
+++ test/napolleon/http_request_maker/http_request_maker_test.exs
@@ -20,8 +20,7 @@ defmodule Napolleon.HTTPRequestMakerTest do
       :url => "example.com"
     }
 
-    {:ok, response} =
-      Napolleon.HTTPRequestMaker.make_request_closure(from: payload_input).()
+    {:ok, response} = Napolleon.HTTPRequestMaker.make_request_closure(from: payload_input).()
 
     assert response.status_code == 200
   end
